e70e6cf06f0a512241f893b78d8df70f4b916269
kenoaljiji <keno.a89@gmail.com>
2023-04-13 23:59:49 +0200

    first commit

---
+++ src/sections/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className='container'>
+      <h2 className={classes.home}>Welcome home</h2>
+    </div>
+  );
+};
+
+export default Home;
